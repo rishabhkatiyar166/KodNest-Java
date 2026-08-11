2d40271586bdb3e67ffad389e682036eca14b18c
Rishabh Katiyar <rishabh9807208651@gmail.com>
2026-08-11 08:04:43 +0530

    Update first Java program structure and add pseudocode

---
+++ track/M02-introduction-to-java/T01-basic-programming-constructs/M02-T01-ST08-L-first-java-program/work/pseducode.java
@@ -0,0 +1,7 @@
+public class pseducode {
+    public static void main(String[] args) {
+        System.out.println("Java: 10");
+        System.out.println("Aptitude: 5");
+        System.out.println("Total: 5");
+    }
+}
