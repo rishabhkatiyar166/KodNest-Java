cef4506819a84e963e12f89e4e96b43bfca5eeab
Rishabh Katiyar <rishabh9807208651@gmail.com>
2026-08-12 08:20:18 +0530

    declare

---
+++ track/M02-introduction-to-java/T01-basic-programming-constructs/M02-T01-ST08-L-first-java-program/work/declare.java
@@ -0,0 +1,8 @@
+public class declare {
+    public static void main(String[] args) {
+        String learnerName = "Rishabh";
+        int dailyPracticeHours = 8;
+        System.out.println("Name:" + learnerName);
+        System.out.println("Daily Practice:" + dailyPracticeHours + "Hours");
+    }
+}
