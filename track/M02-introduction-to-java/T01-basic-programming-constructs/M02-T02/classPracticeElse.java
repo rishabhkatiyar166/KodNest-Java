253c834aa9609665d9a18754b9621036eb1cfb23
Rishabh Katiyar <rishabh9807208651@gmail.com>
2026-08-14 09:33:07 +0530

    add

---
+++ track/M02-introduction-to-java/T01-basic-programming-constructs/M02-T02/classPracticeElse.java
@@ -0,0 +1,13 @@
+public class classPracticeElse {
+    public static void main(String[] args) {
+        int number = 25;
+
+        if (number > 0) {
+            System.out.println("Number type: Positive");
+        } else if (number < 0) {
+            System.out.println("Number type: Negative");
+        } else {
+            System.out.println("Number type: Zero");
+        }
+    }
+}
