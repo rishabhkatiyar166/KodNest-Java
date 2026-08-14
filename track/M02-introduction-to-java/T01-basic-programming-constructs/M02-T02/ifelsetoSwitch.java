f5f893f59f4dab007e88939fd1e919e5f9e9fa34
Rishabh Katiyar <rishabh9807208651@gmail.com>
2026-08-14 15:39:23 +0530

    Add

---
+++ track/M02-introduction-to-java/T01-basic-programming-constructs/M02-T02/ifelsetoSwitch.java
@@ -0,0 +1,30 @@
+public class ifelsetoSwitch {
+    public static void main(String[] args) {
+        int day = 6;
+        switch (day) {
+            case 1:
+                System.out.println("Learning Day");
+                break;
+            case 2:
+                System.out.println("Learning Day");
+                break;
+            case 3:
+                System.out.println("Learning Day");
+                break;
+            case 4:
+                System.out.println("Learning Day");
+                break;
+            case 5:
+                System.out.println("Learning Day");
+                break;
+            case 6:
+                System.out.println("Revision Day");
+                break;
+            case 7:
+                System.out.println("Revision Day");
+                break;
+            default:
+                System.out.println("Invalid Day");
+        }
+    }
+}
