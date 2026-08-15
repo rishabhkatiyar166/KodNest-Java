47850ccaf96e27fb9b1d61f1b0deee3a88ed773a
Rishabh Katiyar <rishabh9807208651@gmail.com>
2026-08-15 20:38:07 +0530

    add

---
+++ track/M02-introduction-to-java/T01-basic-programming-constructs/M02-T02/whilenestedLoop.java
@@ -0,0 +1,14 @@
+public class whilenestedLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            int j = 1;
+            while (j <= 5) {
+                System.out.print(j);
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
