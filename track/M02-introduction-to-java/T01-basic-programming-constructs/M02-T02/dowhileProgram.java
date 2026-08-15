5b89c6c845aefb5dffdaf8e47dcf83739d5e1ebe
Rishabh Katiyar <rishabh9807208651@gmail.com>
2026-08-15 18:28:40 +0530

    add

---
+++ track/M02-introduction-to-java/T01-basic-programming-constructs/M02-T02/dowhileProgram.java
@@ -0,0 +1,9 @@
+public class dowhileProgram {
+    public static void main(String[] args) {
+        int count = 1;
+        do {
+            System.out.println("Day" + count + "is progress day.");
+            count++;
+        } while (count <= 6);
+    }
+}
