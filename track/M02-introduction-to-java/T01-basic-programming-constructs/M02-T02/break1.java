6c4586e419ce3665a721d2456364b1ce8776a321
Rishabh Katiyar <rishabh9807208651@gmail.com>
2026-08-16 10:45:42 +0530

    Add

---
+++ track/M02-introduction-to-java/T01-basic-programming-constructs/M02-T02/break1.java
@@ -0,0 +1,11 @@
+public class break1 {
+    public static void main(String[] args) {
+        int i;
+        for (i = 1; i <= 10; i++) {
+            if (i == 6) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
---
+++ track/M02-introduction-to-java/T01-basic-programming-constructs/M02-T02/continue1.java
@@ -0,0 +1,11 @@
+public class continue1 {
+    public static void main(String[] args) {
+        int i;
+        for (i = 1; i <= 5; i++) {
+            if (i == 3) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
